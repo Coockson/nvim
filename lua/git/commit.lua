0aa0d27c3e92f9e4505da540edbe8f599a1cb3ae
Coockson <khaancc@gmail.com>
2023-09-16 22:21:17 +0200

    test: commit input

---
+++ lua/git/commit.lua
@@ -0,0 +1,37 @@
+local Input = require("nui.input")
+local event = require("nui.utils.autocmd").event
+
+local input = Input({
+  position = "50%",
+  size = {
+    width = 30,
+  },
+  border = {
+    style = "single",
+    text = {
+      top = "[Commit Message]",
+      top_align = "center",
+    },
+  },
+  win_options = {
+    winhighlight = "Normal:Normal,FloatBorder:Normal",
+  },
+}, {
+  prompt = "> ",
+  default_value = "",
+  on_close = function()
+    print("Input Closed!")
+  end,
+  on_submit = function(value)
+    print("Input Submitted: " .. value)
+	vim.fn.system({"git", "commit", "-am", value})
+  end,
+})
+
+-- mount/open the component
+input:mount()
+
+-- unmount component when cursor leaves buffer
+input:on(event.BufLeave, function()
+  input:unmount()
+end)
