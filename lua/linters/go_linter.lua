b24b42b477e6a8c4ec19ce7b9564be52e1212bf4
khaancc <khaancc@gmail.com>
2023-12-14 18:27:13 +0100

    feat: go linter

---
+++ lua/linters/go_linter.lua
@@ -0,0 +1,16 @@
+-- This autocommand lints python files after saving and refreshes the page.
+-- Uses black linter
+
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = vim.api.nvim_create_augroup("go_linter", { clear = true }),
+	pattern = "*.go",
+	callback = function()
+		vim.fn.jobstart({"gofmt", "-w", vim.api.nvim_buf_get_name(0)},{
+			stdout_buffered = true,
+			on_stdout = function(_, data)
+				vim.api.nvim_command('checktime')
+			end
+		})
+	end,
+})
